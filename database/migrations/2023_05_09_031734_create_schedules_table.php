88edc9da61832494c653af2a05c7652dd21df65e
MUHAMMAD-WARIS\waris <wariszargardev@gmail.com>
2023-05-09 08:31:44 +0500

    Add model & migration for Attendence module

---
+++ database/migrations/2023_05_09_031734_create_schedules_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('employee_id');
+            $table->unsignedBigInteger('location_id');
+            $table->unsignedBigInteger('shift_id');
+            $table->date('date');
+            $table->timestamps();
+
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->foreign('location_id')->references('id')->on('locations');
+            $table->foreign('shift_id')->references('id')->on('shifts');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
---
+++ database/migrations/2023_05_09_031806_create_attendences_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendences', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('schedule_id');
+            $table->dateTime('check_in')->nullable();
+            $table->dateTime('check_out')->nullable();
+            $table->timestamps();
+
+            $table->foreign('schedule_id')->references('id')->on('schedules');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendences');
+    }
+}
